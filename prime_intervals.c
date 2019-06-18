bbbbb988577d0c2eb18aa13b36895201287eabd0
saikrishna379 <34342593+saikrishna379@users.noreply.github.com>
2019-06-18 15:57:44 +0530

    Create prime_intervals.c

---
+++ prime_intervals.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num1,num2,i,flag;
+    scanf("%d",&num1);
+    scanf("%d",&num2);
+    while(num1<num2)
+    {
+        flag=0;
+            for(i=2;i<=num1/2;i++)
+            {
+                if(num1%i==0)
+                {
+                    flag=1;
+                    break;
+                }
+            }
+            if(flag==0)
+            {
+                printf("%d\t",num1);
+            }
+            ++num1;
+    }
+return 0;
+}
