1c713c428fb9fd366165b768e7fb8ca2751a8295
saikrishna379 <34342593+saikrishna379@users.noreply.github.com>
2019-07-16 09:03:29 +0530

    Update suminap.py

--- suminap.py
+++ suminap.py
@@ -2,3 +2,5 @@ N,A,D=map(int,input().split())
 if((N>=1) and (N<=10000) and (A>=1) and (A<=10000) and (D>=1) and (D<=10000)):
     sum=(N/2)*((2*A)+(N-1)*D)
     print(int(sum))
+elif(D==0):
+    print(A)
