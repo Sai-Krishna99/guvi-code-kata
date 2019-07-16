e4412e884c2384e27fe26ddbd44990f3b6007f48
saikrishna379 <34342593+saikrishna379@users.noreply.github.com>
2019-07-16 09:59:41 +0530

    Update greatestinarr.py

--- greatestinarr.py
+++ greatestinarr.py
@@ -1,7 +1,4 @@
+n=int(input())
 arr=list(map(int,input().split()))
-l=len(arr)
-large=arr[0]
-for i in range(1,l):
-    if (large<arr[i]):
-        large=arr[i]
-print(large)
+arr.sort()
+print(arr[-1])
