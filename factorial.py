8532ef3157e5f8df334a4ea2d379a3d88bc7ecff
saikrishna379 <34342593+saikrishna379@users.noreply.github.com>
2019-07-17 14:06:44 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,5 +1,5 @@
-number=int(input())
-factorial=1
-for i in range(1,number+1):
-    factorial=factorial*i
-print(factorial)
+num1=int(input())
+max=1
+for i in range(1,num1+1):
+    max=max*i
+print(max)
