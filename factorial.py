45faed78e70404ac70578d71b278d07d58d7301c
saikrishna379 <34342593+saikrishna379@users.noreply.github.com>
2019-07-17 14:04:11 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,5 +1,5 @@
-a=int(input())
-fact=1
-for i in range(1,a+1):
-    fact=fact*i
-print(fact)
+number=int(input())
+factoral=1
+for i in range(1,number+1):
+    factorial=factorial*i
+print(factorial)
